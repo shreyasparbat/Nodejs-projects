3ce1564b2f41a04ca06cf369db3322c872c44b49
Unknown <shreyasp.2016@sis.smu.edu.sg>
2018-05-18 10:42:34 -0700

    Started express app

--- 3. ExpressWebServer/ExpressWebServer/index.js
+++ 3. ExpressWebServer/ExpressWebServer/index.js
@@ -1,6 +1,9 @@
+const hbs = require('hbs');
 const express = require('express');
-
 var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.send({
@@ -10,6 +13,16 @@ app.get('/', (req, res) => {
             'and',
             'that'
         ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
     });
 });
 
